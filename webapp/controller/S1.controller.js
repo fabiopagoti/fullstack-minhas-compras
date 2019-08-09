03ea14f83b4764fef1952a6da2397cc8b2914998
Fábio Luiz Esperati Pagoti <fabiopagoti@gmail.com>
2019-08-09 13:09:40 +0000

    primeiro release

--- webapp/controller/S1.controller.js
+++ webapp/controller/S1.controller.js
@@ -1,11 +1,36 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter"
+	], function (Controller, Filter) {
 	"use strict";
-
 	return Controller.extend("hayamax.aluno0.minhas_compras.controller.S1", {
-		onInit: function () {
+		onInit: function () {},
+		onItemPress: function (oEvent) {
+			var oParameters = oEvent.getParameters();
+			var oListItem = oParameters.listItem;
+			// nao tem ( )
+			var oHeaderContext = oListItem.getBindingContext();
+			var oHeader = oHeaderContext.getObject();
+			var sKey = oHeader.PoKey;
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("detail", {
+				id: sKey
+			});
+		},
 
+		onSearch: function (oEvt) {
+			// add filter for search
+			var aFilters = [];
+			var sQuery = oEvt.getSource().getValue();
+			if (sQuery && sQuery.length > 0) {
+				var filter = new Filter("PoId", sap.ui.model.FilterOperator.StartsWith, sQuery);
+				aFilters.push(filter);
+			}
+
+			// update list binding
+			var list = this.byId("list");
+			var binding = list.getBinding("items");
+			binding.filter(aFilters, "Application");
 		}
 	});
 });
---
+++ webapp/controller/S2.controller.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("hayamax.aluno0.minhas_compras.controller.S2", {
+
+		onInit: function () {
+
+			this.getOwnerComponent()
+				.getRouter()
+				.getRoute("detail")
+				.attachPatternMatched(this.onPatternMatched, this);
+		},
+
+		onPatternMatched: function (oEvent) {
+			var oParameters = oEvent.getParameters();
+			var oArguments = oParameters.arguments; // sem ( )
+			var sPoKey = oArguments.id; // parametro da rota (manifest.json e navTo)
+
+			var oDataModel = this.getOwnerComponent().getModel();
+			
+			var that = this; 
+			oDataModel.metadataLoaded().then(function () {
+
+				var sPath = oDataModel.createKey("Headers", {
+					PoKey: sPoKey
+				});
+
+				// @type sap.ui.core.mvc.View
+				var oView = that.getView();
+				oView.bindElement("/" + sPath);
+
+			});
+		}
+
+	});
+
+});
